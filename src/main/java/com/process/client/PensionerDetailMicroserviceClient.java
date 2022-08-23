6e38b474fe746e4a199ddbec3b4cb8b39aac2ef5
Manu1609 <77477188+Manu1609@users.noreply.github.com>
2022-08-23 17:07:54 +0530

    Update PensionerDetailMicroserviceClient.java

--- src/main/java/com/process/client/PensionerDetailMicroserviceClient.java
+++ src/main/java/com/process/client/PensionerDetailMicroserviceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.process.entity.PensionerDetail;
 
-@FeignClient(name = "pensionDetailMicroservice",url="http://67.202.34.208:8080/pension")
+@FeignClient(name = "pensionDetailMicroservice",url="http://54.164.57.42:8080/pension")
 public interface PensionerDetailMicroserviceClient {
 
 	
